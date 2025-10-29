95967cc89ea085f2bf5f57b200230e636d1d26fc
vonglor patou <vonglorpatou@vonglors-Laptop.local>
2025-10-29 18:46:00 +0700

    rename index

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
